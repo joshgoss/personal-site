ce8c7804376c7b6846f30af586c5a6bbfbe9cb23
Joshua Goss <josh@joshgoss.com>
2023-10-13 12:30:06 -0400

    Tweak styling to be brighter and less dark

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,19 +18,18 @@ export default function Home() {
       </Head>
 
       {/* Header */}
-      <header className="border-b border-gray-900 p-3 ml-2 bg-gray-900">
-        <span className="text-xl text-gray-300">Joshua </span>
-        <span className="text-xl text-gray-400">Goss</span>
-        <span> - </span>
+      <header className="border-b border-gray-300 p-3 bg-white">
+        <span className="text-xl text-gray-700 ml-2">Joshua </span>
+        <span className="text-xl text-gray-500">Goss</span>
       </header>
 
-      <section className="border-b border-gray-900 bg-gray-700 pt-3  pb-5 px-4">
-        <h1 className="text-4xl text-white ml-6 pt-3 mb-4">
+      <section className="border-b border-gray-300 bg-white pt-3  pb-8 px-4">
+        <h1 className="text-4xl text-gray-800 ml-6 pt-3 mb-4">
           <span className="block">Hi,</span>
-          <span className="text-red-400 block">I&apos;m Josh,</span>
-          <span className="block">Full Stack Developer,</span>
+          <span className="text-red-500 block">I&apos;m Josh,</span>
+          <span className="block">Full Stack Developer</span>
         </h1>
-        <p className="text-gray-300 ml-6">
+        <p className="text-gray-500 ml-6">
           I&apos;m a Full Stack Developer with 4+ years of experience. I&apos;m
           well-versed in a range of technologies, including Python and Node.js
           for backend development, React for creating engaging frontends, and
@@ -38,7 +37,7 @@ export default function Home() {
           transforming ideas into functional, user-friendly web applications.
         </p>
 
-        <div className="text-gray-400 ml-10 mt-5 mb-3 ">
+        <div className="text-gray-500 ml-10 mt-5 mb-3 ">
           <a className="mr-3" href="https://github.com/joshgoss">
             <i className="fa-brands fa-github mr-1" />
             View Github
@@ -52,26 +51,26 @@ export default function Home() {
       </section>
 
       {/* Skills */}
-      <section className="border-b  border-gray-900">
-        <h2 className="text-gray-100 ml-7 mb-5 text-lg pt-3">My Skills</h2>
+      <section className="border-b  border-gray-300 bg-gray-100">
+        <h2 className="text-gray-700 ml-4 mb-5 text-lg pt-3">My Skills</h2>
 
         <div className="flex gap-4 ml-6 pb-5 mr-4 flex-col md:flex-row">
-          <div className="flex text-gray-200 flex-initial lg:w-80 w-full ml-3">
-            <i className="fa fa-desktop text-[44px] mr-4" />
+          <div className="flex  flex-initial lg:w-80 w-full ml-3">
+            <i className="fa fa-desktop text-[44px] mr-4 text-gray-600" />
             <div>
-              <h3 className="font-bold  mb-1 w-32">Full Stack</h3>
-              <p className="text-gray-400 text-sm">
+              <h3 className="font-bold  mb-1 w-32 text-gray-600">Full Stack</h3>
+              <p className="text-gray-500 text-sm">
                 With over 4 years in Full Stack Development, I&apos;m skilled in
                 Python, Node.js, React, HTML, and CSS. From backend to frontend,
                 I create seamless web experiences.
               </p>
             </div>
           </div>
-          <div className="flex text-gray-200 flex-initial lg:w-80 w-full ml-3">
-            <i className="fa fa-brands fa-python text-[57px] mr-4" />
+          <div className="flex flex-initial lg:w-80 w-full ml-3">
+            <i className="fa fa-brands fa-python text-[57px] mr-4 text-gray-600" />
             <div>
-              <h3 className="font-bold  mb-1">Python</h3>
-              <p className="text-gray-400 text-sm">
+              <h3 className="font-bold  mb-1 text-gray-600">Python</h3>
+              <p className="text-gray-500 text-sm">
                 I&apos;ve leveraged Python&apos;s capabilities to build
                 efficient backends using Flask and FastAPI. This include complex
                 RESTful backend APIs using SQLAlchemy for database interactions
@@ -79,21 +78,21 @@ export default function Home() {
               </p>
             </div>
           </div>
-          <div className="flex text-gray-200 flex-initial lg:w-80 w-full ml-3">
-            <i className="fa fa-brands fa-node-js text-[57px]  mr-4" />
+          <div className="flex  flex-initial lg:w-80 w-full ml-3">
+            <i className="fa fa-brands fa-node-js text-[57px]  mr-4 text-gray-600" />
             <div>
-              <h3 className="font-bold  mb-1">Node.JS</h3>
-              <p className="text-gray-400 text-sm">
+              <h3 className="font-bold  mb-1 text-gray-600">Node.JS</h3>
+              <p className="text-gray-500 text-sm">
                 Significant experience creating RESTful Node.js APIs and custom
                 scripts for data processing or integrating with cloud services.
               </p>
             </div>
           </div>
-          <div className="flex text-gray-200 flex-initial lg:w-80 w-full ml-3">
-            <i className="fa fa-brands fa-react text-[50px] mr-4" />
+          <div className="flex flex-initial lg:w-80 w-full ml-3">
+            <i className="fa fa-brands fa-react text-[50px] mr-4  text-gray-600" />
             <div>
-              <h3 className="font-bold  mb-1">React</h3>
-              <p className="text-gray-400 text-sm">
+              <h3 className="font-bold  mb-1  text-gray-600">React</h3>
+              <p className="text-gray-500 text-sm">
                 I bring a solid four years of expertise in React development to
                 the table, during which I&apos;ve honed my skills in crafting
                 exceptional user interfaces and optimizing overall user
@@ -103,11 +102,11 @@ export default function Home() {
               </p>
             </div>
           </div>
-          <div className="flex text-gray-200 flex-initial lg:w-80 w-full ml-3">
-            <i className="fa fa-cloud text-[40px] mr-4" />
+          <div className="flex flex-initial lg:w-80 w-full ml-3">
+            <i className="fa fa-cloud text-[40px] mr-4  text-gray-600" />
             <div>
-              <h3 className="font-bold  mb-1">Cloud Services</h3>
-              <p className="text-gray-400 text-sm">
+              <h3 className="font-bold  mb-1  text-gray-600">Cloud Services</h3>
+              <p className="text-gray-500 text-sm">
                 Experience with hosting front-end and backend on AWS and Azure
                 and integrating their servies. This includes AWS EC2, AWS S3,
                 AWS SQS, Azur Blob Storage and Azure App Services.
@@ -117,9 +116,9 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="bg-gray-800 pb-8 mb-8">
+      <section className="bg-gray-700 pb-8 border-b border-gray-800">
         <div className="flex flex-row flex-wrap gap-3 mx-4">
-          <h2 className="text-lg text-gray-100 ml-6 pt-2 mb pb-3 block w-full">
+          <h2 className="text-lg text-gray-200 pt-3 mb pb-3 block w-full">
             Credentials
           </h2>
           <a
@@ -163,8 +162,8 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="bg-gray-100 pb-8">
-        <h2 className="text-lg ml-6 pt-2 mb pb-3">Projects</h2>
+      <section className="bg-gray-800 pb-8">
+        <h2 className="text-lg ml-4 pt-2 mb pb-3 text-gray-200">Projects</h2>
 
         <div className="flex flex-row flex-wrap gap-3 mx-4">
           <Project
